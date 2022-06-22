c9880c7c3e9fa3bfb79528903e409ff38372ce5d
emhogann <ehogan.usgmail.com>
2022-06-22 16:44:08 -0400

    added files

---
+++ Fortune Teller Activity/fortune3.js
@@ -0,0 +1,10 @@
+var fortune = 8
+if (fortune >= 0 && fortune <= 3){
+    console.log("sorry! you have a low fortune");
+}
+else if (fortune > 3 && fortune <= 7){
+    console.log("you have an average fortune!");
+}
+else if (fortune > 7 && fortune <= 10){
+    console.log("you have great fortune!");
+}
